50acc03d678930001750220d729b544fdc2604da
STEIN197 <nil20122013@gmail.com>
2020-04-08 19:20:56 +0500

    Changed lib folder structure

--- src/test/java/site/stein197/b64/EncoderTest.java
+++ src/test/java/site/stein197/b64/EncoderTest.java
@@ -1,5 +1,21 @@
 package site.stein197.b64;
 
+import org.junit.Test;
+
 public class EncoderTest {
-	
+
+	@Test(expected = IllegalArgumentException.class)
+	public void newInstance_OnEmptyString_ThrowsException() {
+		new Encoder("", false);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void newInstance_OnNull_ThrowsException() {
+		new Encoder(null, false);
+	}
+
+	public void encode_OnNotExistingFile_ThrowsException() {}
+	public void encode_OnDirectory_ThrowsException() {}
+	public void encode_IfOnlyb64_ReturnsOnlyB64() {}
+	public void encode_WhenOnlyB64IsFalse_ReturnsCSSURL() {}
 }
