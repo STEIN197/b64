5c3ba895f0466191337818052bf7c329f85711ce
STEIN197 <nil20122013@gmail.com>
2020-04-12 17:56:19 +0500

    Ant runs multiple tests

--- src/test/java/site/stein197/b64/ApplicationTest.java
+++ src/test/java/site/stein197/b64/ApplicationTest.java
@@ -1,5 +1,16 @@
 package site.stein197.b64;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import picocli.CommandLine;
+
 public class ApplicationTest {
-	
+
+	@Test
+	public void nullNames_Returns3() {
+		var app = new Application();
+		new CommandLine(app).parseArgs("");
+		assertEquals(new Integer(3), app.execute().getKey());
+	}
 }
